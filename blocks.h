769d2a10e4c98baa9f0c6c1937138b21ae9e247f
Tifeko <106326856+Tifeko@users.noreply.github.com>
2024-06-08 11:24:57 +0200

    Added icons

--- blocks.h
+++ blocks.h
@@ -1,10 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Cpu:", "cpuu", 30,   0},
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{":", "cpuu", 15,   0},
+	{":", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	15,		0},
 
-	{"", "date '+%b %d (%a) %H:%M'",					5,		0},
+	{"", "date '+%b %d (%a) %H:%M:%S'",					1,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
